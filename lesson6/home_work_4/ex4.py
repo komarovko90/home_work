80776d83a866d2745e786894e09dc9670def9ad1
komarovko90_work <komarov.ko90@gmail.com>
2020-01-28 12:32:46 +0300

    Correct work 4

--- lesson6/home_work_4/ex4.py
+++ lesson6/home_work_4/ex4.py
@@ -23,43 +23,31 @@ class Car:
         print(self.name, self.color)
 
 class TownCar(Car):
-    def __init__(self, speed, col, name):
-        super().__init__(speed, col, name)
-
     def show_speed(self):
         if self.speed > 60:
             print(f'Over speed!! {self.speed}')
             print(f'Police rides after {self.name}!')
-            self.is_police = True
         else:
             print(f'Your speed: {self.speed}')
 
 
 class WorkCar(Car):
-    def __init__(self, speed, col, name):
-        super().__init__(speed, col, name)
-
     def show_speed(self):
         if self.speed > 40:
             print (f'Over speed!! {self.speed}')
             print(f'Police rides after {self.name}!')
-            self.is_police = True
         else:
             print(f'Your speed: {self.speed}')
 
 
 class SportCar(Car):
-    def __init__(self, speed, col, name):
-        super().__init__(speed, col, name)
-
+    pass
 
 class PoliceCar(Car):
-    def __init__(self, speed, col, name):
-        super().__init__(speed, col, name)
-
+    pass
 
 vesta = TownCar(35, 'red', 'Vesta')
-police = PoliceCar(100, 'blue', 'Police')
+police = PoliceCar(100, 'blue', 'Police', True)
 porsche = SportCar(110, 'black', 'porsche')
 reno = WorkCar(40, 'yellow', 'Reno')
 
@@ -69,14 +57,18 @@ vesta.show_speed()
 vesta.stop()
 vesta.show_speed()
 vesta.turn('left')
+print(vesta.is_police)
 print('-'*40)
 police.info_car()
 police.show_speed()
+print(police.is_police)
 print('-'*40)
 porsche.info_car()
 porsche.show_speed()
+print(porsche.is_police)
 print('-'*40)
 reno.info_car()
 reno.show_speed()
 reno.go(45)
 reno.show_speed()
+print(reno.is_police)
